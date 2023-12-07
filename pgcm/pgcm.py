794341a90d6aec8a7543dd559975ed41ee5b7dff
Jaehwa Han <drh@wafflestudio.com>
2023-12-07 19:50:15 +0900

    add index metric

--- pgcm/pgcm.py
+++ pgcm/pgcm.py
@@ -187,6 +187,99 @@ query_bgwriter_buffers_backend = 'select buffers_backend from pg_stat_bgwriter;'
 query_bgwriter_maxwritten_clean = 'select maxwritten_clean from pg_stat_bgwriter;'
 query_oldest_mxid="SELECT max(mxid_age(datminmxid)) oldest_current_mxid FROM pg_database ;"
 query_autovacuum_multixact_freeze_max_age= "select setting::float AS autovacuum_multixact_freeze_max_age FROM pg_catalog.pg_settings WHERE name = 'autovacuum_multixact_freeze_max_age';"
+query_index_stat = """SELECT current_database(), nspname AS schemaname, tblname, idxname, bs*(relpages)::bigint AS real_size,
+  bs*(relpages-est_pages)::bigint AS extra_size,
+  round((100 * (relpages-est_pages)::float / relpages)::numeric, 2) AS extra_pct,
+  fillfactor,
+  CASE WHEN relpages > est_pages_ff
+    THEN bs*(relpages-est_pages_ff)
+    ELSE 0
+  END AS bloat_size,
+  round((100 * (relpages-est_pages_ff)::float / relpages)::numeric, 2) AS bloat_pct,
+  is_na
+FROM (
+  SELECT coalesce(1 +
+         ceil(reltuples/floor((bs-pageopqdata-pagehdr)/(4+nulldatahdrwidth)::float)), 0
+      ) AS est_pages,
+      coalesce(1 +
+         ceil(reltuples/floor((bs-pageopqdata-pagehdr)*fillfactor/(100*(4+nulldatahdrwidth)::float))), 0
+      ) AS est_pages_ff,
+      bs, nspname, tblname, idxname, relpages, fillfactor, is_na
+  FROM (
+      SELECT maxalign, bs, nspname, tblname, idxname, reltuples, relpages, idxoid, fillfactor,
+            ( index_tuple_hdr_bm +
+                maxalign - CASE
+                  WHEN index_tuple_hdr_bm %% maxalign = 0 THEN maxalign
+                  ELSE index_tuple_hdr_bm %% maxalign
+                END
+              + nulldatawidth + maxalign - CASE
+                  WHEN nulldatawidth = 0 THEN 0
+                  WHEN nulldatawidth::integer %% maxalign = 0 THEN maxalign
+                  ELSE nulldatawidth::integer %% maxalign
+                END
+            )::numeric AS nulldatahdrwidth, pagehdr, pageopqdata, is_na
+      FROM (
+          SELECT n.nspname, i.tblname, i.idxname, i.reltuples, i.relpages,
+              i.idxoid, i.fillfactor, current_setting('block_size')::numeric AS bs,
+              CASE
+                WHEN version() ~ 'mingw32' OR version() ~ '64-bit|x86_64|ppc64|ia64|amd64' THEN 8
+                ELSE 4
+              END AS maxalign,
+              24 AS pagehdr,
+              16 AS pageopqdata,
+              CASE WHEN max(coalesce(s.null_frac,0)) = 0
+                  THEN 2
+                  ELSE 2 + (( 32 + 8 - 1 ) / 8)
+              END AS index_tuple_hdr_bm,
+              sum( (1-coalesce(s.null_frac, 0)) * coalesce(s.avg_width, 1024)) AS nulldatawidth,
+              max( CASE WHEN i.atttypid = 'pg_catalog.name'::regtype THEN 1 ELSE 0 END ) > 0 AS is_na
+          FROM (
+              SELECT ct.relname AS tblname, ct.relnamespace, ic.idxname, ic.attpos, ic.indkey, ic.indkey[ic.attpos], ic.reltuples, ic.relpages, ic.tbloid, ic.idxoid, ic.fillfactor,
+                  coalesce(a1.attnum, a2.attnum) AS attnum, coalesce(a1.attname, a2.attname) AS attname, coalesce(a1.atttypid, a2.atttypid) AS atttypid,
+                  CASE WHEN a1.attnum IS NULL
+                  THEN ic.idxname
+                  ELSE ct.relname
+                  END AS attrelname
+              FROM (
+                  SELECT idxname, reltuples, relpages, tbloid, idxoid, fillfactor, indkey,
+                      pg_catalog.generate_series(1,indnatts) AS attpos
+                  FROM (
+                      SELECT ci.relname AS idxname, ci.reltuples, ci.relpages, i.indrelid AS tbloid,
+                          i.indexrelid AS idxoid,
+                          coalesce(substring(
+                              array_to_string(ci.reloptions, ' ')
+                              from 'fillfactor=([0-9]+)')::smallint, 90) AS fillfactor,
+                          i.indnatts,
+                          pg_catalog.string_to_array(pg_catalog.textin(
+                              pg_catalog.int2vectorout(i.indkey)),' ')::int[] AS indkey
+                      FROM pg_catalog.pg_index i
+                      JOIN pg_catalog.pg_class ci ON ci.oid = i.indexrelid
+                      WHERE ci.relam=(SELECT oid FROM pg_am WHERE amname = 'btree')
+                      AND ci.relpages > 0
+                  ) AS idx_data
+              ) AS ic
+              JOIN pg_catalog.pg_class ct ON ct.oid = ic.tbloid
+              LEFT JOIN pg_catalog.pg_attribute a1 ON
+                  ic.indkey[ic.attpos] <> 0
+                  AND a1.attrelid = ic.tbloid
+                  AND a1.attnum = ic.indkey[ic.attpos]
+              LEFT JOIN pg_catalog.pg_attribute a2 ON
+                  ic.indkey[ic.attpos] = 0
+                  AND a2.attrelid = ic.idxoid
+                  AND a2.attnum = ic.attpos
+            ) i
+            JOIN pg_catalog.pg_namespace n ON n.oid = i.relnamespace
+            JOIN pg_catalog.pg_stats s ON s.schemaname = n.nspname
+                                      AND s.tablename = i.attrelname
+                                      AND s.attname = i.attname
+            GROUP BY 1,2,3,4,5,6,7,8,9,10,11
+      ) AS rows_data_stats
+  ) AS rows_hdr_pdg_stats
+) AS relation_stats
+where nspname in """ + schema_list + """
+and tblname in """ + tables_list +"""
+ORDER BY bloat_Size desc, nspname, tblname, idxname; """
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -749,6 +842,8 @@ def handler(event, context):
         logger_debug.debug("Executing result_autovacuum_multixact_freeze_max_age")
         result_autovacuum_multixact_freeze_max_age = executeSQL(conn, query_autovacuum_multixact_freeze_max_age)
         logger_debug.debug("result_autovacuum_multixact_freeze_max_age = " + str(result_autovacuum_multixact_freeze_max_age[0][0]))
+
+        result_index_stat = executeSQL(conn, query_index_stat)
 
         logger.info("------------------------------")
         logger.info("the queries execution finished")
@@ -1523,6 +1618,141 @@ def handler(event, context):
                 ],
                 Namespace='PG Counter Metrics'
             )
+
+            for row in result_index_stat:
+                tblname = row[2]
+                idxname = row[3]
+                real_size = row[4]
+                extra_size = row[5]
+                extra_pct = row[6]
+                bloat_size = row[8]
+                bloat_pct = row[9]
+
+                cloudwatch.put_metric_data(
+                    MetricData=[
+                        {
+                            'MetricName': 'index_stat_real_size',
+                            'Dimensions': [
+                                {
+                                    'Name': 'DBInstanceIdentifier',
+                                    'Value': rds_config.metric_name
+                                },
+                                {
+                                    'Name': 'TableName',
+                                    'Value': tblname
+                                },
+                                {
+                                    'Name': 'IndexName',
+                                    'Value': idxname
+                                },
+                            ],
+                            'Unit': 'Bytes',
+                            'Value': real_size
+                        },
+                    ],
+                    Namespace='PG Counter Metrics'
+                )
+
+                cloudwatch.put_metric_data(
+                    MetricData=[
+                        {
+                            'MetricName': 'index_stat_extra_size',
+                            'Dimensions': [
+                                {
+                                    'Name': 'DBInstanceIdentifier',
+                                    'Value': rds_config.metric_name
+                                },
+                                {
+                                    'Name': 'TableName',
+                                    'Value': tblname
+                                },
+                                {
+                                    'Name': 'IndexName',
+                                    'Value': idxname
+                                },
+                            ],
+                            'Unit': 'Bytes',
+                            'Value': extra_size
+                        },
+                    ],
+                    Namespace='PG Counter Metrics'
+                )
+
+                cloudwatch.put_metric_data(
+                    MetricData=[
+                        {
+                            'MetricName': 'index_stat_extra_pct',
+                            'Dimensions': [
+                                {
+                                    'Name': 'DBInstanceIdentifier',
+                                    'Value': rds_config.metric_name
+                                },
+                                {
+                                    'Name': 'TableName',
+                                    'Value': tblname
+                                },
+                                {
+                                    'Name': 'IndexName',
+                                    'Value': idxname
+                                },
+                            ],
+                            'Unit': 'Percent',
+                            'Value': extra_pct
+                        },
+                    ],
+                    Namespace='PG Counter Metrics'
+                )
+
+                cloudwatch.put_metric_data(
+                    MetricData=[
+                        {
+                            'MetricName': 'index_stat_bloat_size',
+                            'Dimensions': [
+                                {
+                                    'Name': 'DBInstanceIdentifier',
+                                    'Value': rds_config.metric_name
+                                },
+                                {
+                                    'Name': 'TableName',
+                                    'Value': tblname
+                                },
+                                {
+                                    'Name': 'IndexName',
+                                    'Value': idxname
+                                },
+                            ],
+                            'Unit': 'Bytes',
+                            'Value': bloat_size
+                        },
+                    ],
+                    Namespace='PG Counter Metrics'
+                )
+
+                cloudwatch.put_metric_data(
+                    MetricData=[
+                        {
+                            'MetricName': 'index_stat_bloat_pct',
+                            'Dimensions': [
+                                {
+                                    'Name': 'DBInstanceIdentifier',
+                                    'Value': rds_config.metric_name
+                                },
+                                {
+                                    'Name': 'TableName',
+                                    'Value': tblname
+                                },
+                                {
+                                    'Name': 'IndexName',
+                                    'Value': idxname
+                                },
+                            ],
+                            'Unit': 'Percent',
+                            'Value': bloat_pct
+                        },
+                    ],
+                    Namespace='PG Counter Metrics'
+                )
+
         logger_debug.debug("starting  cloudwatch.put_metric_data.oldest_open_transaction")
         cloudwatch.put_metric_data(
             MetricData=[
